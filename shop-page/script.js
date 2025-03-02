85421d5a8b0121a22b681d6421a25b8558b376e3
soztalha <23050111069@aybu.edu.tr>
2025-03-02 06:25:13 +0300

    added cart js code

--- shop-page/script.js
+++ shop-page/script.js
@@ -1,13 +1,16 @@
 const liste = document.getElementsByClassName("add_button");
-
 const listem = Array.from(liste);
-    listem.forEach((element) =>{
-    element.addEventListener(onclick, sepeteEkle);
+let cartIcon = document.getElementById("cartAmount");
+let count = 0;
+
+listem.forEach((element) => {
+    element.addEventListener("click", sepeteEkle);
 });
 
-function sepeteEkle(e) {
-    ; 
+function sepeteEkle() {
+    if (count < 9) {
+        count++;
+        cartIcon.textContent = count;
+        cartIcon.style.backgroundColor = count === 9 ? "red" : "yellow";
+    }
 }
-
-let cartIcon = document.getElementById("cartAmount");
-// cartIcon.innerHTML= ;
